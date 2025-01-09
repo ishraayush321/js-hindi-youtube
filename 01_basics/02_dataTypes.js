bf56ea2a987b1ca2466a02a5a1185182900d26af
ishraayush321 <87688675+ishraayush321@users.noreply.github.com>
2025-01-09 11:27:34 +0000

    Learn diff datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all Js code as newer version
+
+//alert("hello") //We are using nodejs not browser
+
+console.log(3
+    +      //code readability should be high
+3);
+
+let name ="Aarav" //String datatype
+let age = 23 //Number datatype
+let isLoggedIn = false //Boolean datatype
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
